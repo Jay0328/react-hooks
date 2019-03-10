be2eaad4033350d88f399ae32d2c7c6d77f584a0
Jay <gcobs135102@gmail.com>
2019-03-10 11:57:23 +0800

    refactor(lifecycle): separate didMount and unmount
    BREAKING CHANGE: useLifeCycles is removed, use useDidMount and useUnmount instead

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
+export { default as useDidMount } from './useDidMount';
 export { default as useElement } from './useElement';
-export { default as useLifeCycle } from './useLifeCycle';
 export { default as usePrevious } from './usePrevious';
 export { default as useStateCb } from './useStateCb';
+export { default as useUnmount } from './useUnmount';
---
+++ src/useDidMount.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function useDidMount(didMount: () => void) {
+	React.useEffect(didMount, []);
+}
