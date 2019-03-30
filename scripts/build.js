3a9eaf5ae72642ddb7aa8c0ea5b296517d6f894a
Jay <gcobs135102@gmail.com>
2019-03-30 13:55:54 +0800

    chore: upgrade
    no longer support commonjs
    
    BREAKING CHANGE: use es instead of commonjs

--- scripts/build.js
+++ scripts/build.js
@@ -1,37 +1,17 @@
 const path = require('path');
 const { rollup } = require('rollup');
 const babel = require('rollup-plugin-babel');
-const { uglify } = require('rollup-plugin-uglify');
-const { terser } = require('rollup-plugin-terser');
 const { exec } = require('child_process');
 const entries = require('./entries');
 
 const root = path.resolve(__dirname, '..');
-const onlyStripCommentsOptions = {
-	compress: false,
-	mangle: false,
-	output: {
-		comments: false,
-		beautify: true,
-	},
-};
+
 const buildDeclaration = async dir => {
 	await exec(`tsc --noEmit false --emitDeclarationOnly --declaration --declarationDir ${dir}`);
 };
 
-const buildCommonJS = async bundle => {
-	const dir = path.resolve(root, 'cjs');
-	await bundle.write({
-		dir,
-		format: 'cjs',
-		sourcemap: false,
-		interop: false
-	});
-	await buildDeclaration(dir);
-};
-
 const buildES = async bundle => {
-	const dir = path.resolve(root, 'es');
+	const dir = root;
 	await bundle.write({
 		dir,
 		format: 'es',
@@ -43,7 +23,7 @@ const buildES = async bundle => {
 
 const build = async () => {
 	const input = await entries();
-	const babelPlugin = babel({
+	const plugins = babel({
 		exclude: 'node_modules/**',
 		extensions: ['.ts', '.js']
 	});
@@ -64,24 +44,11 @@ const build = async () => {
 	];
 
 	await Promise.all([
-		buildCommonJS(
-			await rollup({
-				input,
-				external,
-				plugins: [
-					babelPlugin,
-					uglify(onlyStripCommentsOptions)
-				]
-			})
-		),
 		buildES(
 			await rollup({
 				input,
 				external,
-				plugins: [
-					babelPlugin,
-					terser(onlyStripCommentsOptions)
-				]
+				plugins
 			})
 		)
 	]);
--- scripts/build.remove.js
+++ scripts/build.remove.js
@@ -4,16 +4,31 @@ const { exec } = require('child_process');
 const root = path.resolve(__dirname, '..');
 const src = path.resolve(root, 'src');
 
-const removeCommonJS = async () => {
-	await exec(`rm -rf ${path.resolve(root, 'cjs')}`);
-};
-
 const removeES = async () => {
-	await exec(`rm -rf ${path.resolve(root, 'es')}`);
+	await Promise.all(
+		await fs
+			.readdirSync(path.resolve(src))
+			.map(async dir => {
+				const stat = await fs.statSync(path.resolve(src, dir));
+
+				if (stat.isDirectory()) {
+					return exec(`rm -rf ${path.resolve(root, dir)}`)
+				}
+
+				const filename = dir
+					.split('.')
+					.slice(0, -1)
+					.join('.');
+
+				return exec(`rm ${path.resolve(root, filename)}.*`);
+			})
+	);
+
+	await exec(`rm ${path.resolve(root, 'index.js')}`);
+	await exec(`rm ${path.resolve(root, 'index.d.ts')}`);
 };
 
 const removeBuild = async () => await Promise.all([
-	removeCommonJS(),
 	removeES(),
 ]);
 
