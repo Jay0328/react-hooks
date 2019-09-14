c250d71433198fc7ce8a4824514c8a66ce84d1fd
Jay Chen <gcobs135102@gmail.com>
2019-09-14 10:17:10 +0800

    docs(useDidMount): add comments

--- src/useDidMount.ts
+++ src/useDidMount.ts
@@ -1,5 +1,10 @@
 import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
 
+/**
+ * Async callback is acceptable.
+ * 
+ * Use layout effect since fired in the same phase as `componentDidMount`.
+ */
 export function useDidMount(didMount: () => void) {
 	useIsomorphicLayoutEffect(() => {
 		didMount();
