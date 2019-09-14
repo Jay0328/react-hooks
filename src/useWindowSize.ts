5923f60d42a2e60b1dabecf7a9ddb3b7b8a6a4a2
Jay Chen <gcobs135102@gmail.com>
2019-09-14 10:42:35 +0800

    refactor(useWindowSize): use name export
    BREAKING CHANGE: use name export

--- src/useWindowSize.ts
+++ src/useWindowSize.ts
@@ -1,34 +1,33 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 export interface WindowSize {
 	width: number;
 	height: number;
 }
 
-export default function useWindowSize({ width = Infinity, height = Infinity } = {}) {
+export function useWindowSize({ width = Infinity, height = Infinity } = {}) {
 	const isClient = typeof window === 'object';
-	const [state, setState] = React.useState<WindowSize>({
+	const [state, setState] = useState<WindowSize>(() => ({
 		width: isClient ? window.innerWidth : width,
 		height: isClient ? window.innerHeight : height,
-	});
+	}));
 
-	React.useEffect(
-		() => {
-			if (isClient) {
-				const onResize = () => {
-					setState({
-						width: window.innerWidth,
-						height: window.innerHeight,
-					});
-				};
-				window.addEventListener('resize', onResize);
-				return () => window.removeEventListener('resize', onResize);
-			} else {
-				return undefined;
-			}
-		},
-		[]
-	);
+	useEffect(() => {
+		if (!isClient) {
+			return;
+		}
+
+		function onResize() {
+			setState({
+				width: window.innerWidth,
+				height: window.innerHeight,
+			});
+		}
+
+		window.addEventListener('resize', onResize);
+
+		return () => window.removeEventListener('resize', onResize);
+	}, []);
 
 	return state;
 }
