817631df5f7b7727f9df1e137d51d8befd7a3fc2
Jay Chen <gcobs135102@gmail.com>
2019-09-14 09:51:44 +0800

    chore: add transform-runtime to babel config

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,14 @@
 module.exports = api => {
-	const test = api.env('test');
+	api.cache(true);
+
 	return {
 		presets: [
-			[
-				'@babel/preset-env',
-				{
-					loose: true,
-					useBuiltIns: 'entry',
-					modules: test ? 'commonjs' : false,
-				},
-			],
-			"@babel/preset-react",
+			['@babel/preset-env', { loose: true }],
+			'@babel/preset-react',
 			'@babel/preset-typescript'
 		],
 		plugins: [
-			"@babel/plugin-proposal-json-strings"
+			'@babel/plugin-transform-runtime'
 		],
 	};
 };
