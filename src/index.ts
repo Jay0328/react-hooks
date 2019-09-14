d5463ffbad56ac63893430a0d8eb0eab4faeb576
Jay Chen <gcobs135102@gmail.com>
2019-09-14 09:50:21 +0800

    refactor: remove entry file
    BREAKING CHANGE: entry file removed, import  hook file needed instead

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as useDidMount } from './useDidMount';
-export { default as useElement } from './useElement';
-export { default as useMediaQuery } from './useMediaQuery';
-export { default as usePrevious } from './usePrevious';
-export { default as useStateCb } from './useStateCb';
-export { default as useUnmount } from './useUnmount';
-export { default as useDidUpdate } from './useDidUpdate';
-export { default as useWindowSize } from './useWindowSize';
