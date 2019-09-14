4ddfb6a1713a242887b0b3a7a86e12c4ebe3bb9c
Jay Chen <gcobs135102@gmail.com>
2019-09-14 10:36:50 +0800

    fix(useDidUpdate): avoid from triggered on did mount phase

--- src/useDidUpdate.ts
+++ src/useDidUpdate.ts
@@ -1,22 +1,24 @@
-import React from 'react';
+import { useRef } from 'react';
+import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
 
-export default function useDidUpdate(didUpdate: () => void, deps?: ReadonlyArray<any>) {
-	const isMounted = React.useRef(false);
+/**
+ * Async callback is acceptable.
+ * 
+ * Use layout effect since fired in the same phase as `componentDidUpdate`.
+ * 
+ * The reason of not using isMounted hook provided by this project is since
+ * the effect of isMounted must invoked before the layout effect of useDidUpdate.
+ * So event if just on the did mount phase, the isMounted.current will always be true.
+ */
+export function useDidUpdate(didUpdate: () => void, deps?: ReadonlyArray<any>) {
+	const isMounted = useRef(false);
 
-	React.useEffect(
-		() => {
-			isMounted.current = true;
-			return () => {
-				isMounted.current = false;
-			};
-		},
-		[]
-	);
-
-	React.useEffect(
+	useIsomorphicLayoutEffect(
 		() => {
 			if (isMounted.current) {
 				didUpdate();
+			} else {
+				isMounted.current = true;
 			}
 		},
 		deps
